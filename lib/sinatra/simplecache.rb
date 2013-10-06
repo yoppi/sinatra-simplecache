1ec5153b56c1188112bfe13e9a330de01d5b33bb
yoppi <y.hirokazu@gmail.com>
2013-10-06 09:12:33 +0900

    initial commit

---
+++ lib/sinatra/simplecache.rb
@@ -0,0 +1,7 @@
+require "sinatra/simplecache/version"
+
+module Sinatra
+  module Simplecache
+    # Your code goes here...
+  end
+end
---
+++ lib/sinatra/simplecache/version.rb
@@ -0,0 +1,5 @@
+module Sinatra
+  module Simplecache
+    VERSION = "0.0.1"
+  end
+end
